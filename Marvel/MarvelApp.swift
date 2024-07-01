a7c5b30dec1ba1443483ac6053c7d598db246174
Luis <luis.fernandomunoz@plexus.es>
2024-07-01 13:56:13 +0200

    Initial Commit

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Luis Fernando Sanchez Muñoz on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
